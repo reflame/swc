da3f28e53ed7608dcb2613e4f4864a2690c9a819
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-10-31 17:51:59 -0700

    Update tests with snapshots

--- crates/swc_ecma_transforms_react/src/refresh_setup/tests.rs
+++ crates/swc_ecma_transforms_react/src/refresh_setup/tests.rs
@@ -21,40 +21,6 @@ import * as blah from './blah.js'
 export const blah_test = 12345
 
 export const blah = 1234
-"#,
-    // Output codes after transformed with plugin
-    r#"
-import.meta.url = new URL("/hi.js", location.origin)
-const $reflamePathname = new URL(import.meta.url).pathname
-const $reflamePreviousRefreshReg = self.$RefreshReg$
-const $reflamePreviousRefreshSig = self.$RefreshSig$
-self.$RefreshReg$ = (type, id) => {
-    const fullId = $reflamePathname + ` ${id}`
-    self.$reflame.reactRefreshRuntime.register(type, fullId)
-}
-self.$RefreshSig$ = self.$reflame.reactRefreshRuntime.createSignatureFunctionForTransform
-
-import * as blah from './blah.js'
-
-export const blah_test = 12345
-
-export const blah = 1234
-
-self.$RefreshReg$ = $reflamePreviousRefreshReg
-self.$RefreshSig$ = $reflamePreviousRefreshSig
-
-self.$reflame.registerAcceptCallback({ 
-  pathname: $reflamePathname,
-  callback: ({ pathname, id }) => {
-    if (id) {
-      console.debug("accepting", pathname, "to", id)
-    } else {
-      console.debug("accepting", pathname)
-    }
-
-    self.$reflame.performReactRefresh()
-  }, 
-})
 "#
 );
 
@@ -66,14 +32,6 @@ test!(
     |_| refresh_setup(false, None,),
     disabled,
     // Input codes
-    r#"
-import * as blah from './blah.js'
-
-export const blah_test = 12345
-
-export const blah = 1234
-"#,
-    // Output codes after transformed with plugin
     r#"
 import * as blah from './blah.js'
 
---
+++ crates/swc_ecma_transforms_react/tests/__swc_snapshots__/src/refresh_setup/tests.rs/disabled.js
@@ -0,0 +1,5 @@
+import * as blah from './blah.js'
+
+export const blah_test = 12345
+
+export const blah = 1234
